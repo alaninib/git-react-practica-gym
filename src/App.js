73a27e10ccb5ea7fefc32a110a1b04531ba4ebe4
alaninib <alaninib.dev@gmail.com>
2022-06-11 15:20:22 -0400

    Agrega assets

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { Box } from '@mui/material'
+
+import './App.css';
 import Home from './pages/Home';
 import ExerciseDetail from './pages/ExerciseDetail';
-import Navbar from './components/Navbar'
-import './App.css';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App(){
 
   return(
-    <Box>
+    <Box width="400px" sx={{width: {xl:'1488px' }}} m="auto">
       <Navbar />
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/exercise/:id' element={<ExerciseDetail/>}/>
       </Routes>
+      <Footer />
     </Box>
   )
 }
